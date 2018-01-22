6134ee68872d35196ea77cf79781d5fa3a9caa69
Arthur Guiot <arguiot@gmail.com>
2018-01-22 18:21:46 +0800

    Added pearson correlation Using Excel documentation

--- __test__/theorem.js
+++ __test__/theorem.js
@@ -187,6 +187,23 @@ class TheoremJS {
   	const average = new BigNumber(summed).div(arguments.length);
   	return average
   }
+  correlation(array1, array2) {
+      if (array1.length != array2.length) {
+          throw "[TheoremJS]: Correlation error, arrays are not the same size"
+      }
+      const average1 = this.average(...array1).toNumber()
+      const average2 = this.average(...array2).toNumber()
+      let up = 0;
+  	let down1 = 0;
+  	let down2 = 0;
+      for (var i = 0; i < array1.length; i++) {
+          up += (array1[i] - average1)*(array2[i] - average2)
+  		down1 += Math.pow(array1[i] - average1, 2)
+  		down2 += Math.pow(array2[i] - average2, 2)
+      }
+  	const result = up / Math.sqrt(down1 * down2)
+  	return result
+  }
   median() {
   	let array = [...arguments]
   	array.sort( (a, b) => new BigNumber(a).sub(b) );
