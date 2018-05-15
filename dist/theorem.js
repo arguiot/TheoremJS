9c9aef2240dddbf4b4f333348d05c48a8000c8b1
arguiot <arguiot@gmail.com>
2018-05-15 21:17:52 +0800

    Fixed root

--- dist/theorem.js
+++ dist/theorem.js
@@ -106,7 +106,7 @@ class TheoremJS {
       }
   	let result = []
       for (var i = 0; i < n.length; i++) {
-      	result.push(new BigNumber(Math.pow(new BigNumber(n[i]).toNumber(), new BigNumber(1).div(base).toNumber())))
+      	result.push(new BigNumber(Math.pow(new BigNumber(n[i]).toNumber(), new BigNumber(1).div(base).toNumber()).toPrecision(15)))
       }
   	return result.length == 1 ? result[0] : result
   }
--- src/includes/functions/math/math/root.js
+++ src/includes/functions/math/math/root.js
@@ -4,7 +4,7 @@ root(n, base) {
     }
 	let result = []
     for (var i = 0; i < n.length; i++) {
-    	result.push(new BigNumber(Math.pow(new BigNumber(n[i]).toNumber(), new BigNumber(1).div(base).toNumber())))
+    	result.push(new BigNumber(Math.pow(new BigNumber(n[i]).toNumber(), new BigNumber(1).div(base).toNumber()).toPrecision(15)))
     }
 	return result.length == 1 ? result[0] : result
 }
