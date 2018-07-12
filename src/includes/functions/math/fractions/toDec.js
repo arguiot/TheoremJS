fc1791addd06f12fd126498fb1c70908ac4c88f3
Arthur Guiot <arguiot@gmail.com>
2018-07-12 13:44:07 +0200

    Fixed toDec in fractions

--- src/includes/functions/math/fractions/toDec.js
+++ src/includes/functions/math/fractions/toDec.js
@@ -1,5 +1,11 @@
 toDec() {
 	const args = [...arguments]
+	if (typeof args[0] == 'object') {
+		if (args[0].length != 2) {
+			throw "Require 2 numbers"
+		}
+		return new BigNumber(args[0][0]).div(args[0][1])
+	}
 	if (args.length != 2) {
 		throw "Require 2 numbers"
 	}
