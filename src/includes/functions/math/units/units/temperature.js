5503835e7031a6d6f64f6f3546d1eb2176127751
Arthur Guiot <arguiot@gmail.com>
2018-07-19 13:44:22 +0200

    Added temperature

---
+++ src/includes/functions/math/units/units/temperature.js
@@ -0,0 +1,27 @@
+temperature(v, a, b) {
+	const authorized = [
+		"c",
+		"f",
+		"k"
+	]
+	if (!authorized.includes(a) || !authorized.includes(b)) {
+		throw "[TheoremJS] Speed: wrong units"
+	}
+	const ia = authorized.indexOf(a)
+	const ib = authorized.indexOf(b)
+	// to celsius
+	const add = [
+		0,
+		-32,
+		-273.15
+	]
+	const factor = [
+		1,
+		new BigNumber(5).div(9),
+		1
+	]
+	const g = new BigNumber(v).plus(add[ia]).times(factor[ia])
+
+	const out = g.div(factor[ib]).minus(add[ib])
+	return out
+}
