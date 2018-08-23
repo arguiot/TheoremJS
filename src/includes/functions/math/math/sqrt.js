b83e89c04702a29aa829befd2d92f2a0ad99dd10
Arthur Guiot <arguiot@gmail.com>
2018-08-23 15:26:14 +0200

    Adding support of complex number on sqrt (#2)

--- src/includes/functions/math/math/sqrt.js
+++ src/includes/functions/math/math/sqrt.js
@@ -4,7 +4,11 @@ sqrt(n) {
     }
 	let result = []
     for (var i = 0; i < n.length; i++) {
-    	result.push(new BigNumber(new BigNumber(n[i]).sqrt()))
+		if (new BigNumber(n[i]).it(0)) {
+			result.push(this.complex(0, new BigNumber(n[i]).abs().sqrt()))
+		} else {
+			result.push(new BigNumber(new BigNumber(n[i]).sqrt()))
+		}
     }
 	return result.length == 1 ? result[0] : result
 }
