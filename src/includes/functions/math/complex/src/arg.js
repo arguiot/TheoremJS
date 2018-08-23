9e6654360a548d318f90df97de78b3d340008bbf
Arthur Guiot <arguiot@gmail.com>
2018-08-23 13:43:48 +0200

    Ajustement on arg (#2)

--- src/includes/functions/math/complex/src/arg.js
+++ src/includes/functions/math/complex/src/arg.js
@@ -1,6 +1,7 @@
-arg(complex) {
+arg() {
 	if (!complex.isComplex) {
 		throw "[TheoremJS]: Complex operation require complex numbers"
 	}
 	return new BigNumber(this.t.atan2(this.a, this.b))
+	return this
 }
