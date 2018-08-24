857bbf24cd08f15793d25c2434c904b0c82ceb49
Arthur Guiot <arguiot@gmail.com>
2018-08-24 21:51:20 +0200

    Improved times (#2)

--- src/includes/functions/math/complex/src/times.js
+++ src/includes/functions/math/complex/src/times.js
@@ -5,7 +5,11 @@ times(complex) {
 	if (!complex.isComplex) {
 		throw "[TheoremJS]: Complex operation require complex numbers"
 	}
-	this.a = this.a.times(complex.a)
-	this.b = this.b.times(complex.b)
+	this.a = this.a.times(complex.a).minus(this.b.times(complex.b))
+	this.b = this.a
+				.plus(this.b)
+				.times(complex.a.plus(complex.b))
+				.minus(this.a.times(complex.a))
+				.minus(this.b.times(complex.b)) // 	(a+b)(c+d)-ac-bd.
 	return this
 }
