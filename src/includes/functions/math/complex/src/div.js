0df9a8df1afaf476142c1ca9b0b33f4086673732
Arthur Guiot <arguiot@gmail.com>
2018-08-24 21:16:48 +0200

    Improved division (#2)

--- src/includes/functions/math/complex/src/div.js
+++ src/includes/functions/math/complex/src/div.js
@@ -5,7 +5,24 @@ div(complex) {
 	if (!complex.isComplex) {
 		throw "[TheoremJS]: Complex operation require complex numbers"
 	}
-	this.a = this.a.div(complex.a)
-	this.b = this.b.div(complex.b)
-	return this
+	if (complex.b.eq(0)) {
+		this.a = this.a.div(complex.a)
+		this.b = this.b.div(complex.b)
+		return this
+	}
+	if (complex.a.abs().lt(complex.b.abs())) {
+		const x = complex.a.div(complex.b);
+        const t = complex.a.times(x).plus(complex.b);
+
+		this.a = this.a.times(x).plus(this.b).div(t)
+		this.b = this.b.times(x).minus(this.a).div(t)
+		return this
+	} else {
+		const x = complex.b.div(complex.a);
+        const t = complex.b.times(x).plus(complex.a);
+
+		this.a = this.b.times(x).plus(this.a).div(t)
+		this.b = this.b.minus(this.a.times(x)).div(t)
+		return this
+	}
 }
