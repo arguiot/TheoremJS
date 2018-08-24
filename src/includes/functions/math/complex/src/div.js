9af59a59825b64010b7b877631f81eb95a3b59ac
Arthur Guiot <arguiot@gmail.com>
2018-08-24 21:24:51 +0200

    Improved (again) division (#2) http://mathworld.wolfram.com/ComplexDivision.html

--- src/includes/functions/math/complex/src/div.js
+++ src/includes/functions/math/complex/src/div.js
@@ -5,24 +5,22 @@ div(complex) {
 	if (!complex.isComplex) {
 		throw "[TheoremJS]: Complex operation require complex numbers"
 	}
-	if (complex.b.eq(0)) {
-		this.a = this.a.div(complex.a)
-		this.b = this.b.div(complex.b)
-		return this
-	}
-	if (complex.a.abs().lt(complex.b.abs())) {
-		const x = complex.a.div(complex.b);
-        const t = complex.a.times(x).plus(complex.b);
+	
+	const a = this.a
+	const b = this.b
+	const c = complex.a
+	const d = complex.b
 
-		this.a = this.a.times(x).plus(this.b).div(t)
-		this.b = this.b.times(x).minus(this.a).div(t)
-		return this
-	} else {
-		const x = complex.b.div(complex.a);
-        const t = complex.b.times(x).plus(complex.a);
+	const c2d2 = c.times(c).plus(d.times(d)) // c^2 + d^2
 
-		this.a = this.b.times(x).plus(this.a).div(t)
-		this.b = this.b.minus(this.a.times(x)).div(t)
-		return this
-	}
+	const acbd = a.times(c).plus(b.times(d)) // a*c + b*d
+	const bcad = b.times(c).minus(a.times(d)) // b*c - a*d
+
+	const re = acbd.div(c2d2)
+	const im = bcad.div(c2d2)
+
+	this.a = re
+	this.b = im
+
+	return this
 }
