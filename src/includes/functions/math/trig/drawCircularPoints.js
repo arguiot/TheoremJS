ba59d6e811ce8472d62e452fab5f09f14febffd9
Arthur Guiot <arguiot@gmail.com>
2018-08-26 18:08:01 +0200

    Updated drawCircularPoints to support complex (#2)

--- src/includes/functions/math/trig/drawCircularPoints.js
+++ src/includes/functions/math/trig/drawCircularPoints.js
@@ -1,4 +1,4 @@
-drawCircularPoints(n, r=1, start=[-r, 0]) {
+drawCircularPoints(n, r=1, start=[-r, 0], complex=false) {
 	const angle = this.pi().times(2).div(n)
 	let buffer = {}
 	buffer[start[0]] = start[1]
@@ -6,8 +6,15 @@ drawCircularPoints(n, r=1, start=[-r, 0]) {
 	for (var i = 0; i < n - 1; i++) {
 		const x = new BigNumber(r).times(this.cos(angleState)).toString()
 		const y = new BigNumber(r).times(this.sin(angleState)).toNumber()
-		buffer[x] = y
+		if (complex === true) {
+			buffer[i] = this.complex(x, y)
+		} else {
+			buffer[x] = y
+		}
 		angleState += angle.toNumber()
+	}
+	if (complex === true) {
+		return Object.values(buffer)
 	}
 	return buffer
 }
