63a7f60334b1a19590384bf3a722a1fc0ab9be7f
Arthur Guiot <arguiot@gmail.com>
2019-01-27 17:43:27 +0800

    Improved isPrime functions Now it's even faster than previous improvement

--- src/includes/functions/math/numbers/isPrime.js
+++ src/includes/functions/math/numbers/isPrime.js
@@ -1,28 +1,16 @@
 isPrime(n) {
-    n = new BigNumber(n).abs()
-    if (n.lt(2) || n.toNumber() * 0 !== 0) {
+	n = new BigNumber(n).abs()
+	if (n.toNumber() * 0 !== 0) {
 		return false;
 	}
+	if (n.lte(3)) return n.gt(1);
+	if (n.mod(2).eq(0) || n.mod(3).eq(0)) return false;
 
-    let i = 0;
-    let j;
-    let sqrt;
-    let arr = [2, 3, 5];
-
-    for (; i < arr.length; i++) {
-		if (n.eq(arr[i])) return true;
-		if (n.mod(arr[i]).eq(0)) return false;
-	}
-
-    arr = [0, 4, 6, 10, 12, 16, 22, 24];
-    sqrt = n.sqrt();
-
-    for (i = 7; i <= sqrt.toNumber(); i += 30) {
-		for (j=0; j < arr.length; j++) {
-			if (n.mod(i + arr[j]).eq(0)) {
-				return false;
-			}
+	for (let i = 5; n.gte(i * i); i += 6) {
+		if (n.mod(i).eq(0) || n.mod(i + 2).eq(0)) {
+			return false
 		}
 	}
-    return true;
+
+	return true;
 }
